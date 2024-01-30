685af5940d15033bee506a775e0ed8f64ad883e2
c7336680 <srijanchowdasry@gmail.com>
2024-01-30 11:11:31 +0000

    test and personals details

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,17 @@
+public class PersonalDetails
+{
+    public static void main(String[] args)
+    {
+
+        String name = "Tony";
+        String address = "123 Main Street, leeds, west yorkshire, ls6 8tb";
+        int age = 29;
+        String phoneNumber = "555-1234";
+
+        System.out.println("Personal Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
+}
---
+++ WeeklyExercises/1GettingStarted/Test.java
@@ -0,0 +1,7 @@
+public class test {
+    //----------------------------------------------------
+    //  Prints a statement.
+    //----------------------------------------------------
+    public static void man(String[] args) {
+        System.out.bogus(An emergency Broadcast")
+    }
