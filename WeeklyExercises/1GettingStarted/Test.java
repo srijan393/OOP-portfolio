031aed92ebb1aa94b035639783f81085bd4c732e
c7336680 <srijanchowdasry@gmail.com>
2024-02-20 10:39:07 +0000

    Week 4

--- WeeklyExercises/1GettingStarted/Test.java
+++ WeeklyExercises/1GettingStarted/Test.java
@@ -1,7 +1,1 @@
-public class test {
-    //----------------------------------------------------
-    //  Prints a statement.
-    //----------------------------------------------------
-    public static void man(String[] args) {
-        System.out.bogus(An emergency Broadcast")
-    }
+
